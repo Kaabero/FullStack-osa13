289c283ea7c49d0ab252d4aa98e9583df8fe7dda
Katri <katri.kuoppala@helsinki.fi>
2024-04-17 13:54:19 +0300

    Exercise 13.7.

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,21 +6,38 @@ const { Blog } = require('../models')
 const blogFinder = async (req, res, next) => {
     req.blog = await Blog.findByPk(req.params.id)
     next()
-  }
+}
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
+    } else if (error.name === 'TypeError') {
+        return res.status(400).json({ error: error.message })
+    } else if (error.name === 'SequelizeValidationError') {
+        return res.status(400).json({ error: 'ValidationError' })
+    } else {
+        res.status(500).send(`Something went wrong!`)
+    }
+    next(error)
+}
+
 
 router.get('/', async (req, res) => {
     const blogs = await Blog.findAll()
     res.json(blogs)
 })
   
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
     console.log(req.body)
     try {
       const blog = await Blog.create(req.body)
       console.log(blog.toJSON())
       return res.json(blog)
     } catch(error) {
-      return res.status(400).json({ error })
+      next(error)
     }
 })
   
@@ -43,14 +60,16 @@ router.delete('/:id', blogFinder, async (req, res) => {
     }
 })
 
-router.put('/:id', blogFinder, async (req, res) => {
-    if (req.blog) {
-      req.blog.likes = req.body.likes
-      await req.blog.save()
-      res.json(req.blog)
-    } else {
-      res.status(404).end()
+router.put('/:id', blogFinder, async (req, res, next) => {
+    try {
+        req.blog.likes = req.body.likes
+        await req.blog.save()
+        res.json(req.blog)
+    } catch (error) {
+        next(error)
     }
 })
+
+router.use(errorHandler)
 
 module.exports = router

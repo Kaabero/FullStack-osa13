a655e22e8c0f731aaf79f265de73ecdc610e14cc
Katri <katri.kuoppala@helsinki.fi>
2024-04-17 09:15:18 +0300

    Exercises 13.1.-13.3.

---
+++ commands.sql
@@ -0,0 +1,3 @@
+insert into blogs (author, url, title, likes) values ('Dan Abramov', 'www.dan.fi', 'Writing Resilient Components', 1);
+insert into blogs (author, url, title) values ('Martin Fowler', 'www.martin.com', 'Is High Quality Software Worth the Cost?');
+insert into blogs (author, url, title) values ('Robert C. Martin', 'www.robert.fi', 'FP vs. OO List Processing');

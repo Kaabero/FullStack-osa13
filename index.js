cdf540c68cfee74f1fb3476ac698ba5fe8f73a96
Katri <katri.kuoppala@helsinki.fi>
2024-04-17 12:47:03 +0300

    Tehtävät 13.5 ja 13.6

---
+++ index.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+const express = require('express')
+const app = express()
+
+const { PORT } = require('./util/config.js')
+const { connectToDatabase } = require('./util/db.js')
+
+const blogsRouter = require('./controllers/blogs.js')
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
+
+start()
---
+++ models/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+const Blog = require('./blog')
+
+Blog.sync()
+
+module.exports = {
+  Blog
+}
